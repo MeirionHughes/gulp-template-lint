9a8bb5398d38b5ba9ac21af0c10e69958338a535
Meirion Hughes <mhughes@geologging.com>
2016-06-04 13:32:19 +0100

    add testing and update  template-lint

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-var gulp = require('gulp');
+var Stream = require('stream');
 var gutil = require('gulp-util');
-var Linter = require('template-lint').Linter;
-var SelfCloseRule = require('template-lint').SelfCloseRule;
-var ParserRule = require('template-lint').ParserRule;
 
-module.exports = function (rules) {
+var TemplateLint = require('template-lint');
+var Linter = TemplateLint.Linter;
+const defaultRules = TemplateLint.DefaultRules;
 
+module.exports = function(rules, reporter) {
+       
     if (!rules)
-        rules = [
-            new SelfCloseRule(),
-            new ParserRule()];
-
+        rules = defaultRules;
+            
+    if(!reporter)
+        reporter = (error, file)=>{
+             gutil.log(`WARNING: ${error.message} Ln ${error.line} Col ${error.column}`, file);
+        }
+          
     var linter = new Linter(rules);
-
-    function sanitize(file, cb) {
-
-        var html = String(file.contents);
-
+   
+    var stream = new Stream.Transform({objectMode: true});
+    stream._transform = function(file, encoding, cb) {
+        // When null just pass through
+        if (file.isNull()) {
+            this.push(file);
+            cb();
+            return;
+        }   
+         
+        var pathshort = "";
+          
+        if(file.path !=null && file.cwd != null)  
+            pathshort = file.path.substring(file.cwd.length, file.path.Length);
+        
+        var html = String(file.contents); 
+        var self = this;
+        
         linter
-            .lint(html)
-            .then((errors) => {
-                errors.forEach((error) => {
-                    gutil.log(`WARNING: ${error.message}, line: ${error.line}, column: ${error.column}`,
-                        file.path.substring(file.cwd.length, file.path.Length));
-                });
+            .lint(html)        
+            .then((errors) => {           
+                errors.forEach((error) => {reporter(error,pathshort)});
             })
-            .then(() => { cb(null, file) });
-    }
-    return require('event-stream').map(sanitize);
-}
+            .then(() => {
+                self.push(file);              
+                cb(null, file) });
+    };
+    return stream;
+};
